e3313652b4667fe18c15424d44a5b4536dec4008
MomDra <yem9087@koreatech.ac.kr>
2021-05-08 22:18:10 +0900

    Move Truck with Keyboard Input
    Edit PlayerController Script

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
     float speed = 20;
+    float turnSpeed = 45;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float vertialInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertialInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
