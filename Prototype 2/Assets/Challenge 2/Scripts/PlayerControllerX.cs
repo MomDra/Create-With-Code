3738f738b527344ef0160aa695dc600be7d6f0f8
MomDra <yem9087@koreatech.ac.kr>
2021-05-09 11:08:32 +0900

    Import New Assets
    Challenge - Play Fetch

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float time = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && time > 1)
+        {
+            time = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
