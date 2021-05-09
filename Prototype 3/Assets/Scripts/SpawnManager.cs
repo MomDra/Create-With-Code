e5a91b6f37d9e8ac1515f9d69a4e0dc1cad03805
MomDra <yem9087@koreatech.ac.kr>
2021-05-09 14:36:17 +0900

    Spawn obstacles at intervals
    Add SpawnManager script

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obstaclePrefab;
+    Vector3 spawnPos = new Vector3(25, 0, 0);
+    float startDelay = 2;
+    float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
